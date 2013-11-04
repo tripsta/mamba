5954ac46d05dd560f2a9967e680c58a79ad8dc8e
Panos Kountanis <pkountanis@travelplanet24.com>
2013-11-04 14:34:00 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+	name = 'mamba',
+	packages = ['mamba'],
+	version = '0.1.0',
+	description = 'Tripsta python library',
+	author = 'Tripsta SA',
+	author_email = 'developers@travelplanet24.com',
+	url = 'https://github.com/tripsta/mamba',
+	long_description = """\
+Tripsta python library
+
+- Zend-like configuration with .ini files
+- bootstrapping according to environmant
+- Testing classes and Decorators
+- Soap Client
+- Twisted helper classes
+
+Works with python 2.7.x
+"""
+
+)
