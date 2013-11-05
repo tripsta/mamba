c733d39e9f252dde589dab60ad4d549d3de05f38
Panos Kountanis <pkountanis@travelplanet24.com>
2013-11-05 12:44:43 +0200

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name = 'mamba',
 	packages = find_packages(),
-	version = '0.1.0',
+	version = '0.1.1',
 	description = 'Tripsta python library',
 	author = 'Tripsta SA',
 	author_email = 'developers@travelplanet24.com',
