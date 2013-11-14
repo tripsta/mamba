f2857f4deaeeb2bd18434a4f370d1cb2df4e8550
kostas matsoukas <kmatsoukas@travelplanet24.com>
2013-11-14 12:17:43 +0200

    Fix to replace proper namespace

--- mamba/soap/client.py
+++ mamba/soap/client.py
@@ -11,12 +11,17 @@ from suds import WebFault, MethodNotFound
 from suds.plugin import MessagePlugin
 import re
 
+
 class SoapEnvelopeNamespaceFix(MessagePlugin):
+
     def sending(self, context):
-		if re.search('(xmlns:ns)+([0-9])+(="http://schemas.xmlsoap.org/soap/envelope/")', context.envelope):
-			context.envelope = re.sub('(xmlns:ns)+[0-9]+(="http://schemas.xmlsoap.org/soap/envelope/")', '', context.envelope)
-			context.envelope = re.sub('(<ns)+([0-9])+(:)', '<SOAP-ENV:', context.envelope)
-			context.envelope = re.sub('(</ns)+([0-9])+(:)', '</SOAP-ENV:', context.envelope)
+		p = re.compile('(xmlns:ns)+([0-9])+(="http://schemas.xmlsoap.org/soap/envelope/")')
+		match = p.search(context.envelope)
+		if match:
+			ns_index = match.group(2)
+			context.envelope = p.sub('', context.envelope)
+			context.envelope = re.sub('(<ns)+({})+(:)'.format(ns_index), '<SOAP-ENV:', context.envelope)
+			context.envelope = re.sub('(</ns)+({})+(:)'.format(ns_index), '</SOAP-ENV:', context.envelope)
 
 """
 SoapClient just for building requests
@@ -37,28 +42,3 @@ class SoapClient(object):
     """
 	def body(self, methodName, params):
 		return getattr(self.client.service, methodName)(**params)
-
-
-
-
-
-if __name__ == '__main__':
-	from twisted.internet import task
-	from twisted.internet import reactor
-	def runEverySecond():
-		print "a second has passed"
-	def gotBody(request):
-		print request.envelope
-	l = task.LoopingCall(runEverySecond)
-	l.start(0.1)
-	# wsdl = 'file://' + os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../../app/apis/amadeus/liveTest/1ASIWT2FTRP_PDT_07September11.wsdl')
-	wsdl = 'file:///home/johnecon/project/pythepie/pythepie/app/apis/amadeus/liveTest/1ASIWT2FTRP_PDT_07September11.wsdl'
-	soapClient = SoapClient(wsdl)
-	userIdentifier = {'originIdentification': {'sourceOffice':'ATHGR28BL'}, 'originatorTypeCode': 'U', 'originator': 'WSTRPT2F'}
-	dutyCode = {'dutyCodeDetails': {'referenceQualifier': 'DUT', 'referenceIdentifier': 'SU'}}
-	systemDetails = {'organizationDetails': {'organizationId': 'NMC-GREECE'}}
-	passwordInfo = {'dataLength': '10', 'dataType': 'E', 'binaryData': 'Y3JhdWtiZWxpbg=='}
-	soapClient.setHeaders({'Session': {'SequenceNumber': '', 'SessionId': '', 'SecurityToken': ''}})
-	d = soapClient.body('Security_Authenticate', dict(dictdutyCode=dutyCode, userIdentifier=userIdentifier, systemDetails=systemDetails, passwordInfo=passwordInfo))
-	d.addCallback(gotBody)
-	reactor.run()
--- mamba/tests/mamba/soap/test_client.py
+++ mamba/tests/mamba/soap/test_client.py
@@ -1,26 +1,53 @@
-# import sys
-# import os
-from twisted.trial import unittest
-# from mamba.soap.client import SoapClient
-# import suds
-# import fixtures
+import mock
+import os
+import suds
+import sys
+from mamba.test.unittest import TestCase
+from mamba.soap.client import SoapClient, SoapEnvelopeNamespaceFix
 
-# wsdl = 'file://' + os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../../../app/apis/amadeus/liveTest/1ASIWT2FTRP_PDT_07September11.wsdl')
-# testClient = SoapClient(wsdl)
 
-class SoapClientTestCase(unittest.TestCase):
-	pass
-	# def test_init_set_proper_client_options(self):
-	# 	self.assertIsInstance(testClient, SoapClient)
-	# 	assert testClient.client.service is not None
-	# 	sudsClient = testClient.client
-	# 	self.assertTrue(testClient.client.options.nosend)
-	# 	self.assertEquals(suds.client.Client, sudsClient.__class__)
+class TestContext(object):
+	envelope = None
 
-	# def test_send_raise_method_not_found(self):
-	# 	self.assertRaises(suds.MethodNotFound, testClient.body, 'FooBar', {})
+	def __init__(self, envelope):
+		self.envelope = envelope
 
-	# def test_set_headers_set_proper_options(self):
-	# 	headers = {'Session': {'SecurityToken': '', 'SequenceNumber': '', 'SessionId': ''}}
-	# 	testClient.setHeaders(headers)
-	# 	self.assertEquals(headers, testClient.client.options.soapheaders)
+
+class SoapEnvelopeNamespaceFixSendingTestCase(TestCase):
+	fix = None
+
+	def setUp(self):
+		self.fix = SoapEnvelopeNamespaceFix()
+
+	def test_replace_nothing_if_no_match_is_found(self):
+		expected_envelope = "foobar"
+		context = TestContext(expected_envelope)
+		self.fix.sending(context)
+		self.assertEqual(expected_envelope, context.envelope)
+
+	def test_replace_proper_namespace_if_match_is_found(self):
+		envelope = 'xmlns:ns0="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="foobar" <ns1:Body>body1</ns1:Body> <ns0:Body>body0</ns0:Body>'
+		context = TestContext(envelope)
+		self.fix.sending(context)
+		self.assertEqual(' xmlns:ns1="foobar" <ns1:Body>body1</ns1:Body> <SOAP-ENV:Body>body0</SOAP-ENV:Body>', context.envelope)
+
+wsdl = 'file://' + os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../data/dummy.wsdl')
+test_client = SoapClient(wsdl)
+
+
+class SoapClientTestCase(TestCase):
+
+	def test_init_set_proper_client_options(self):
+		self.assertIsInstance(test_client, SoapClient)
+		suds_client = test_client.client
+		self.assertTrue(test_client.client.options.nosend)
+		self.assertEquals(suds.client.Client, suds_client.__class__)
+
+	def test_send_raise_exception_if_method_not_found(self):
+		test_client.client.service = mock.Mock()
+		self.assertRaises(Exception, test_client.body, 'FooBar')
+
+	def test_set_headers_set_proper_options(self):
+		headers = {'Session': {'SecurityToken': '', 'SequenceNumber': '', 'SessionId': ''}}
+		test_client.setHeaders(headers)
+		self.assertEquals(headers, test_client.client.options.soapheaders)
