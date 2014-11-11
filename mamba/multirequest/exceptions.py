cb1ebbba7592ba77fde6067f06329ecaa7ebe745
apostolos sotiropoulos <asotiropoulos@travelplanet24.com>
2014-11-11 12:24:59 +0200

    add transactionId to base exception

--- mamba/multirequest/exceptions.py
+++ mamba/multirequest/exceptions.py
@@ -4,15 +4,17 @@ from copy import deepcopy
 class _BaseException(Exception):
     code = None
 
-    def __init__(self, message):
+    def __init__(self, message, transactionId = None):
         Exception.__init__(self, message)
         if isinstance(message, (Exception,)):
             self.message = decode_unless_unicode(message.message)
+            self.transactionId = decode_unless_unicode(message.transactionId)
         else:
             self.message = decode_unless_unicode(message)
+            self.transactionId = decode_unless_unicode(transactionId)
 
     def to_dict(self):
-        return {"code": self.code, "message": self.message}
+        return {"code": self.code, "message": self.message, "transactionId": self.transactionId}
 
     def __str__(self):
         return self.message.encode("UTF-8")
--- mamba/multirequest/tests/test_exceptions.py
+++ mamba/multirequest/tests/test_exceptions.py
@@ -3,8 +3,12 @@ from mamba.multirequest.exceptions import InternalError
 
 class SessionUnavailableError(InternalError):
     code = 501
-    def __init__(self):
-        InternalError.__init__(self, "STS Session Unavailable")
+    def __init__(self, message = "STS Session Unavailable", transactionId = 4):
+        InternalError.__init__(self, message, transactionId)
+
+class ExceptionTest(Exception):
+    message = 'foo'
+    transactionId = 5
 
 class SessionUnavailableTestCase(unittest.TestCase):
 
@@ -16,3 +20,12 @@ class SessionUnavailableTestCase(unittest.TestCase):
 
     def test_code_is_not_500(self):
         self.assertEquals(self.session_exc.code, 501)
+
+    def test_transactionId_is_set_when_message_not_exception(self):
+        self.assertEquals(self.session_exc.transactionId, '4')
+
+    def test_transactionId_is_set_when_message_exception(self):
+        message = ExceptionTest()
+        message.transactionId = 5
+        self.session_exc = SessionUnavailableError(message)
+        self.assertEquals(self.session_exc.transactionId, '5')
